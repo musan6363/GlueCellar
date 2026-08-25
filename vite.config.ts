f80d0746bd268165f01fe18bfc3e30c3d7c0d9db
msn <msn151336+prog@gmail.com>
2026-08-25 22:54:05 +0900

    PWA設定

--- vite.config.ts
+++ vite.config.ts
@@ -8,25 +8,27 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['icon-192x192.png', 'icon-512x512.png'],
       manifest: {
         name: 'Glue Cellar',
-        short_name: 'Glue',
-        theme_color: '#d4c4b7',
-        background_color: '#d4c4b7',
+        short_name: 'Glue Cellar',
+        description: 'ワインのテイスティング記録アプリ',
+        theme_color: '#2a2a2a',
+        background_color: '#2a2a2a',
         display: 'standalone',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: 'icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
