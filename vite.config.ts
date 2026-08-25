4b2323bfab00664c3134565cb0792d4253814442
msn <msn151336+prog@gmail.com>
2026-08-25 22:18:55 +0900

    icon 設定

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,12 @@ export default defineConfig({
             src: '/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
